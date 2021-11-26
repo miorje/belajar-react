209614b59b0d956812e0eb79ebb0c0e9a73044da
mior <miorpunye@gmail.com>
2021-11-26 10:22:30 +0800

    feat: create components

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,6 +2,6 @@ import React, {FunctionComponent} from "react";
 
 export  const Container:FunctionComponent = (props) => {
     return(
-        <div className="max-w-8xl mx-auto py-2 lg:px-8 sm:px-6 xs:px-5">{props.children}</div>
+        <div className="max-w-8xl mx-auto py-2 p-8">{props.children}</div>
     )
 }
